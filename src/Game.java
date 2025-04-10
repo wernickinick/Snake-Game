53754f8f2d5679637addd6b85cd72bef3e261de1
wernickinick <wernickinick@gmail.com>
2025-04-10 17:05:39 -0400

    Initial Commit

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import java.util.ArrayList;
+import javax.swing.*;
 
-public class Game {
+public class Game extends JPanel{
+
+    private class Tile
+    {
+        int x;
+        int y;
+
+        Tile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    int GameBoardWidth;
+    int GameBoardHeight;
+    int TileSize = 25;
+
+    //Snake
+    Tile Snakehead;
+
+    //Food
+    Tile food;
+
+    Game(int GameBoardWidth, int GameBoardHeight)
+    {
+        this.GameBoardWidth = GameBoardWidth;
+        this.GameBoardHeight = GameBoardHeight;
+        setPreferredSize(new Dimension(this.GameBoardWidth,this.GameBoardHeight));
+        setBackground(Color.BLACK);
+
+        Snakehead = new Tile(5,5);
+
+        food = new Tile(10,10);
+
+    }
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    //Snake
+    public void draw(Graphics g)
+    {
+        //Snake
+        g.setColor(Color.green);
+        g.fillRect(Snakehead.x * TileSize, Snakehead.y * TileSize, TileSize, TileSize);
+
+        //Food
+        g.setColor(Color.red);
+        g.fillRect(food.x * TileSize, food.y * TileSize, TileSize,TileSize); //Left off here!!!
+    }
+
+
+
 }
--- src/Snake.java
+++ src/Snake.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
 public class Snake {
+    public static void main(String [ ] args)
+    {
+        int Width = 600;
+        int Height = Width;
+
+        JFrame Frame = new JFrame("Snake");
+        Frame.setBounds(0,0,Width,Height);
+        Frame.setLocationRelativeTo(null);
+        Frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Frame.setResizable(false);
+
+        Game game = new Game(Width,Height);
+        Frame.add(game);
+        Frame.pack();
+        Frame.setVisible(true);
+
+    }
 }
